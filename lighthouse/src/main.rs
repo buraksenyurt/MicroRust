0ef1348776d660f1e614a5a192635f042a039180
buraksenyurt <burakselimsenyurt@gmail.com>
2025-04-11 15:17:47 +0300

    Attempted first try

--- lighthouse/src/main.rs
+++ lighthouse/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use microbit::{hal::gpio::Level, Board};
+use embedded_hal::digital::OutputPin;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Lighthouse sample is starting...");
+
+    if let Some(board) = Board::take() {
+        let mut relay = board.pins.p0_01.into_push_pull_output(Level::Low);
+
+        loop {
+            relay.set_high().unwrap();
+            delay(1_000_000);
+
+            relay.set_low().unwrap();
+            delay(2_000_000);
+        }
+    }
+
+    loop {
+
+    }
+}
+
+fn delay(count: u32) {
+    for _ in 0..count {
+        cortex_m::asm::nop();
+    }
 }
