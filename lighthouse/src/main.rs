77171188763d4876f0364d8f1dc073f9cb9be692
buraksenyurt <burakselimsenyurt@gmail.com>
2025-04-11 16:14:53 +0300

    Lights up

--- lighthouse/src/main.rs
+++ lighthouse/src/main.rs
@@ -1,36 +1,49 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use microbit::{hal::gpio::Level, Board};
-use embedded_hal::digital::OutputPin;
-use panic_rtt_target as _;
+use nrf52833_pac::Peripherals;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Lighthouse sample is starting...");
+    rprintln!("Lighthouse project started!");
 
-    if let Some(board) = Board::take() {
-        let mut relay = board.pins.p0_01.into_push_pull_output(Level::Low);
+    let p = Peripherals::take().unwrap();
 
-        loop {
-            relay.set_high().unwrap();
-            delay(1_000_000);
+    /*
+        Python örnek kodu ışık ayarı için PIN0'ı kullanır ancak asıl map farklıdır.
 
-            relay.set_low().unwrap();
-            delay(2_000_000);
-        }
-    }
+        micro:bit v2 pin mapping:
+        pin0 -> P0.02
+        pin1 -> P0.03
+        pin2 -> P0.04
+    */
+    p.P0.pin_cnf[2].write(|w| {
+        w.dir().output();
+        w.input().disconnect();
+        w.pull().disabled();
+        w.drive().s0s1();
+        w.sense().disabled()
+    });
 
     loop {
+        rprintln!("Relay ON");
+        p.P0.outset.write(|w| unsafe { w.bits(1 << 2) }); // P0.02 HIGH
 
+        delay(400_000);
+
+        rprintln!("Relay OFF");
+        p.P0.outclr.write(|w| unsafe { w.bits(1 << 2) }); // P0.02 LOW
+
+        delay(800_000);
     }
 }
 
 fn delay(count: u32) {
     for _ in 0..count {
-        cortex_m::asm::nop();
+        nop();
     }
 }
